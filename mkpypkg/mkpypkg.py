c6a94013c3fea4364250535452f6a3fd088e4fd1
Gosmeyer <cgosmeye@GSSLW16051875.ndc.nasa.gov>
2019-06-26 08:50:28 -0400

    Completed and tested making empty package.

--- mkpypkg/mkpypkg.py
+++ mkpypkg/mkpypkg.py
@@ -1,10 +1,11 @@
 """
 """
 
+import datetime
 import os
 import shutil
 
-from mkpypkg_config import mkpypkg_loc #, name, orcid
+from mkpypkg_config import mkpypkg_loc, given_name, family_name, orcid, github_user
 
 def mkpypkg(pkg_name, pkg_loc):
 	"""
@@ -21,8 +22,8 @@ def mkpypkg(pkg_name, pkg_loc):
 
 	# Create setup.py
 	setup_name = os.path.join(pkg_loc, pkg_name, 'setup.py')
-	shutil.copy2(os.path.join(mkpypkg_loc, 'setup.py'), setup_name)
-	#edit_setup(setup_name)
+	open(setup_name, 'a').close()
+	edit_setup(setup_name, pkg_name)
 
 	# Create empty README.md
 	readme_name = os.path.join(pkg_loc, pkg_name, 'README.md')
@@ -30,17 +31,19 @@ def mkpypkg(pkg_name, pkg_loc):
 	edit_readme(readme_name, pkg_name)
 
 	# Create filled MIT license
-	shutil.copy2(os.path.join(mkpypkg_loc, 'LICENSE'), os.path.join(pkg_loc, pkg_name, 'LICENSE'))
+	license_name = os.path.join(pkg_loc, pkg_name, 'LICENSE')
+	shutil.copy2(os.path.join(mkpypkg_loc, 'LICENSE'), license_name)
+	edit_license(license_name)
 
 	# Create filled .gitignore
 	gitignore_name = os.path.join(pkg_loc, pkg_name, '.gitignore')
 	shutil.copy2(os.path.join(mkpypkg_loc, '.gitignore'), gitignore_name)
-	#edit_gitignore(gitignore_name)
+	edit_gitignore(gitignore_name)
 
 	# Create template CITATION.cff
 	citation_name = os.path.join(pkg_loc, pkg_name, 'CITATION.cff')
-	shutil.copy2(os.path.join(mkpypkg_loc, 'CITATION.cff'), citation_name)
-	#edit_citation(citation)
+	open(citation_name, 'a').close()
+	edit_citation(citation_name, pkg_name)
 
 	# End with message on how to push new package to GitHub/Lab
 	print("COMPLETE!")
@@ -60,17 +63,51 @@ def mkpypkg(pkg_name, pkg_loc):
 	print("		git push origin master")
 	print("4. Check GitHub/Lab for your new commits.")
 
-def edit_setup(setup):
-	"""
-	"""
 
-def edit_citation(citation):
+
+def edit_citation(citation, pkg_name):
 	"""
 	"""
+	with open(citation, 'a') as f:
+		f.write("cff-version: 1.0.3\n")
+		f.write("message: If you use this software, please cite it as below.\n")
+		f.write("authors:\n")
+		f.write("  - family-names: {}\n".format(family_name))
+		f.write("    given-names: {}\n".format(given_name))
+		f.write("    orcid: https://orcid.org/{}\n".format(orcid))
+		f.write("title: {}\n".format(pkg_name.upper()))
+		f.write("version: 1.0\n")
+		f.write("doi: N/A\n")
+		f.write("date-released: N/A\n")
 
 def edit_gitignore(gitignore):
 	""" Edit to remove mkpypkg-specific lines.
 	"""
+	gittemp = os.path.join(mkpypkg_loc, 'temp')
+	shutil.move(gitignore, gittemp)
+	f = open(gitignore, 'a')
+	with open(gittemp, 'r') as ftemp:
+		for line in ftemp.readlines():
+			if 'mkpypkg' not in line:
+				f.write(line)
+	f.close()
+	os.remove(gittemp)
+
+def edit_license(license):
+	"""
+	"""
+	year = datetime.datetime.now().year
+	licensetemp = os.path.join(mkpypkg_loc, 'temp')
+	shutil.move(license, licensetemp)
+	f = open(license, 'a')
+	with open(licensetemp, 'r') as ftemp:
+		for line in ftemp.readlines():
+			if 'Copyright' in line:
+				f.write("Copyright (c) {} {}. {}\n".format(year, given_name[0], family_name))
+			else:
+				f.write(line)
+	f.close()
+	os.remove(licensetemp)	
 
 def edit_readme(readme, pkg_name):
 	"""
@@ -78,15 +115,27 @@ def edit_readme(readme, pkg_name):
 	with open(readme, 'a') as f:
 		f.write('# {}\n'.format(pkg_name))
 		f.write("\n")
-		f.write('Useful things here about the `` package!\n'.format(pkg_name))
+		f.write('Useful things here about the `{}` package!\n'.format(pkg_name))
 		f.write("\n")
 		f.write('## Installation\n')
 		f.write("\n")
 		f.write('    Run setup.py:\n')
 		f.write("\n")
-		f.write('\t```\n')
 		f.write('\tpython setup.py develop\n')
-		f.write('\t```\n')
+		f.write("\n")
 
-
+def edit_setup(setup, pkg_name):
+	"""
+	"""
+	with open(setup, 'a') as f:
+		f.write("#!/usr/bin/env python\n")
+		f.write("\n")
+		f.write("from setuptools import find_packages\n")
+		f.write("from setuptools import setup\n")
+		f.write("\n")
+		f.write("setup(name = '{}',\n".format(pkg_name))
+		f.write("      description = 'Describe this package!',\n")
+		f.write("      author = '{} {}',\n".format(given_name, family_name))
+		f.write("      url = 'https://github.com/{}/{}',\n".format(github_user, pkg_name))
+		f.write("      packages = find_packages())\n")
 
